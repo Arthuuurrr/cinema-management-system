e3d11cf7aeee573ee4d5568a346ba11ad9022689
Fernando Consul <fernando.consul386@gmail.com>
2026-05-29 08:52:54 -0300

    Update filmes.py

--- filmes.py
+++ filmes.py
@@ -1,1 +1,18 @@
+def criar_filme():
+    filmes=[]
 
+    while True:
+        filme=input("Digite o nome do filme (ou sair)")
+
+        if filme == "sair":
+            break
+
+        if filme not in filmes:
+            filmes.append(filme)
+    print("\nFilmes Cadastrados:")
+
+    for f in filmes:
+        print("-", f)
+
+
+criar_filme()
