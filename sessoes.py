eda48ff58b07737f6e21e15b7abdb2d2ebc9c13a
Fernando Consul <fernando.consul386@gmail.com>
2026-05-30 20:43:01 -0300

    Update sessoes.py

--- sessoes.py
+++ sessoes.py
@@ -1,1 +1,22 @@
+""" Cadastrar salas
+    Criar sessões
+    Listar sessões
+    Verificar capacidade da sala"""
 
+def cadastrar_salas():
+    sessoes=[]
+
+    while True:
+
+        salas=input("digite o numero da sua sala?(ou sair)")
+
+        if salas == "sair":
+            break
+
+        if salas not in sessoes:
+            sessoes.append(salas)
+
+        else:
+            print("essa sala já existe!")
+
+cadastrar_salas()
