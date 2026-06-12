2679cb991698edc1b6f02e194c7ece8ad0f05f71
Fernando Consul <fernando.consul386@gmail.com>
2026-06-11 21:39:59 -0300

    relatorios.py

--- relatorios.py
+++ relatorios.py
@@ -1,1 +1,20 @@
+""" Mostrar total de ingressos vendidos
+    Calcular faturamento total
+    Exibir filme mais assistido
+    Gerar relatório diário  """
+ 
+while True:
+    print("\n=== RELATÓRIOS ===")
+    print("1 - Total de ingressos vendidos")
+    print("0 - Voltar")
 
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        total_ingressos() #ainda nao fiz o total_ingressos(), mas vai usar ela pra fazer
+
+    elif opcao == "0":
+        break
+
+    else:
+        print("Opção inválida!")
