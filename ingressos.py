d7bf69faccac5cd7f6f4656bdc52c534679fc6ee
guiberger07 <guilhermegberger@gmail.com>
2026-06-15 22:00:49 -0300

    Implement ticket selling functionality
    Added a function to sell tickets and store sales data.

--- ingressos.py
+++ ingressos.py
@@ -1,1 +1,23 @@
 
+vendas = []
+
+def vender_ingresso():
+    print("\n--- Venda de Ingresso ---")
+
+    nome = input("Nome do cliente: ")
+    cpf = input("CPF do cliente: ")
+    filme = input("Nome do filme: ")
+    sessao = input("Horário da sessão: ")
+    assento = input("Assento escolhido: ")
+
+    ingresso = {
+        "nome": nome,
+        "cpf": cpf,
+        "filme": filme,
+        "sessao": sessao,
+        "assento": assento
+    }
+
+    vendas.append(ingresso)
+
+    print("\nIngresso vendido com sucesso!")
