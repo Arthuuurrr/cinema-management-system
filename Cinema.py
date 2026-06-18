a12858a2101bf8326ef8e14fd0527294cde1912c
guivalenca <biliuguilherme@gmail.com>
2026-06-18 19:55:03 -0300

    Add menu functions for ingressos and relatorios

--- Cinema.py
+++ Cinema.py
@@ -1,5 +1,14 @@
+from ingressos import menu_ingressos
+from relatorios import menu_relatorios
+
 """A integração"""
 filmes=[]
 salas=[]
 sessoes=[]
 ingressos=[]
+
+elif opcao == "3":
+    menu_ingressos(ingressos)
+
+elif opcao == "4":
+    menu_relatorios(ingressos)
